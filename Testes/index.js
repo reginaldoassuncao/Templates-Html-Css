6027acdbfefbdb5f923964cd1cedd9c87251bc2c
Reginaldo Assunção <regi.assuncao@hotmail.com>
2022-11-19 17:48:24 -0300

    Create index.js

---
+++ Testes/index.js
@@ -0,0 +1,17 @@
+var funcionarios = [
+    "Larissa",
+    "Renato",
+    "Mariana",
+    "Luciane",
+    "João"
+];
+
+var novos_funcionarios = [
+    "Fabiano",
+    "Lívia"
+];
+
+funcionarios[4] = novos_funcionarios[1];
+funcionarios[2] = novos_funcionarios[0];
+
+console.log(funcionarios);
