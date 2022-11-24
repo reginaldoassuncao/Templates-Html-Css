166f1c0e4ab13ce5a0a5d5e410ea1f466cd6aada
Reginaldo Assunção <regi.assuncao@hotmail.com>
2022-11-24 18:14:56 -0300

    arquivos modificados

--- Testes/index.js
+++ Testes/index.js
@@ -1,10 +1,11 @@
-import entradaDados from "readline-sync"
+var colecao_series_programacao = [
+    { nome: "Breaking Bad",           horario: "21h",     sinopse: "Um professor de química se transforma quando descobre ter um câncer terminal. Daí ele usa suas habilidades a favor do crime"},
+    { nome: "Fargo",                  horario: "22h",     sinopse: "Uma sequência de crimes saem errado e são investigados por uma detetive."},
+    { nome: "Lost",                   horario: "20h",     sinopse: "Um avião cai em uma ilha deserta e logo um grupo de passageiros precisa lutar para sobreviver." },
+    { nome: "Prison Break",           horario: "23h",     sinopse: "Um homem cria um plano para tirar o irmão sentenciado à morte por um suposto assassinato do vice-presidente dos EUA"},
+    { nome: "Black Mirror",           horario: "23h",     sinopse: "Contos de ficção científica que refletem o lado negro da tecnologia, mostrando que nem toda novidade traz só benefícios." },
+    { nome: "Pessoa de interesse ",   horario: "20h",     sinopse: "Um ex-agente da CIA, dado como morto pelo governo dos EUA, é recrutado por um milionário, para um projeto ultrassecreto."},
+    { nome: "Dark",                   horario: "22h",     sinopse: "O desaparecimento de crianças na cidade alemã de Winden remete a acontecimentos idênticos ocorridos há 33 anos e 66 anos."}
+];
 
-let valor_produto = 500;
-let total_parcelas = entradaDados.question("Digite o numero de parcelas: ")
-
-for (let contador =1; contador <= total_parcelas; contador++)
-{   
-    let valor_parcela = valor_produto / contador;
-    console.log("Númer0 de parcelas: " + contador + "- valor de parcela: R$ " +valor_parcela.toFixed(2));
-};
+console.log(colecao_series_programacao[5].sinopse)
