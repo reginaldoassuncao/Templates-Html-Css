9afed5546571a77c5c7aab614caf7af6f30e4692
Reginaldo Assunção <regi.assuncao@hotmail.com>
2022-11-27 13:57:56 -0300

    arquivos adicionados

--- Testes/index.js
+++ Testes/index.js
@@ -1,11 +1,25 @@
-var colecao_series_programacao = [
-    { nome: "Breaking Bad",           horario: "21h",     sinopse: "Um professor de química se transforma quando descobre ter um câncer terminal. Daí ele usa suas habilidades a favor do crime"},
-    { nome: "Fargo",                  horario: "22h",     sinopse: "Uma sequência de crimes saem errado e são investigados por uma detetive."},
-    { nome: "Lost",                   horario: "20h",     sinopse: "Um avião cai em uma ilha deserta e logo um grupo de passageiros precisa lutar para sobreviver." },
-    { nome: "Prison Break",           horario: "23h",     sinopse: "Um homem cria um plano para tirar o irmão sentenciado à morte por um suposto assassinato do vice-presidente dos EUA"},
-    { nome: "Black Mirror",           horario: "23h",     sinopse: "Contos de ficção científica que refletem o lado negro da tecnologia, mostrando que nem toda novidade traz só benefícios." },
-    { nome: "Pessoa de interesse ",   horario: "20h",     sinopse: "Um ex-agente da CIA, dado como morto pelo governo dos EUA, é recrutado por um milionário, para um projeto ultrassecreto."},
-    { nome: "Dark",                   horario: "22h",     sinopse: "O desaparecimento de crianças na cidade alemã de Winden remete a acontecimentos idênticos ocorridos há 33 anos e 66 anos."}
+let alunos = [
+    {id: 1, nome: "Bruna", media: 8},
+    {id: 2, nome: "Laura", media: 7},
+    {id: 3, nome: "José", media: 5},
+    {id: 4, nome: "Guilherme", media: 4},
+    {id: 5, nome: "Rafael", media: 10},
 ];
 
-console.log(colecao_series_programacao[5].sinopse)
+let contador = 0;
+
+while ( contador < alunos.length ) {
+
+    let aluno = alunos[contador];
+
+    contador++;
+
+    if ( aluno.media === 0) {
+        break;
+    }
+
+    console.log("Id: " + aluno.id);
+    console.log("Nome: " + aluno.nome);
+    console.log("Média: " + aluno.media);
+    console.log("\n");
+}
