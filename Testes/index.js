47d22bd7fa9cdcc0554c036bfd1db511c8f61130
Reginaldo Assunção <regi.assuncao@hotmail.com>
2022-12-03 20:58:34 -0300

    Update index.js

--- Testes/index.js
+++ Testes/index.js
@@ -1,20 +1,22 @@
-let status = 0;
-
-switch
-(status)
-{
+let alunos = [
+        {id: 1, nome: "Bruna", media: 8},
+        {id: 2, nome: "Laura", media: 7},
+        {id: 3, nome: "José", media: 5},
+        {id: 4, nome: "Guilherme", media: 4},
+        {id: 5, nome: "Rafael", media: 10}
+    ];
     
-case
- 1:
-        console.log("Ativo");
-        
-break
-;
+    for ( let contador = 0; contador < alunos.length; contador++ ) {
     
-case
- 0:
-        console.log("Inativo");
-        
-break
-;
-}
+        let aluno = alunos[contador];
+    
+        if ( aluno.media < 6 ) {
+            continue;
+        }
+    
+        console.log("Id: " + aluno.id);
+        console.log("Nome: " + aluno.nome);
+        console.log("Média: " + aluno.media);
+        console.log("\n");
+    
+    }
