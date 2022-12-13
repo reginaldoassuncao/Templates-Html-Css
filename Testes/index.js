c3fa66634f9459232a2a31f5baecbeb41a851da6
Reginaldo Assunção <regi.assuncao@hotmail.com>
2022-12-13 20:25:38 -0300

    arquivos modificados

--- Testes/index.js
+++ Testes/index.js
@@ -1,16 +1,41 @@
-let categorias =
+let cursos_idiomas = 
  [
-    {id: 1, nome: "Ficção científica"},
-    {id: 2, nome: "Aventura"},
-    {id: 3, nome: "Comédia"},
-    {id: 4, nome: "Terror"},
-    {id: 5, nome: "Drama"},
-    {id: 6, nome: "Reginaldo"}
+    { nome: "Inglês", preco: 2500, carga_horaria: 160 },
+    { nome: "Espanhol", preco: 1500, carga_horaria: 110 },
+    { nome: "Francês", preco: 3600, carga_horaria: 200 },
+    { nome: "Chinês", preco: 5500, carga_horaria: 400 },
+    { nome: "Alemão", preco: 3800, carga_horaria: 230 }
 ];
 
+for(let curso of cursos_idiomas)
+{
 
+    
+let nome_curso
+ = curso.nome;
+    
+let preco_curso
+ = curso.preco;
+    
+let carga_horaria_curso
+ = curso.carga_horaria;
 
-    console.log(categorias[0].id);
-    console.log(categorias[0].nome);
+    let preco_hora_curso = 
+preco_curso / carga_horaria_curso
+ ;
+
+    console.log("Nome do curso: " + nome_curso);
+    console.log("Carga horária do curso: " + carga_horaria_curso + " horas");
+    console.log("Preço do curso: R$" + preco_curso + ",00");
+
+    
+if(preco_hora_curso >= 15)
+ {
+        console.log("Hora/aula superior ou igual a R$15,00");
+    } else {
+        console.log("Hora/aula inferior a R$15,00");
+    }
 
     console.log("\n");
+
+}
