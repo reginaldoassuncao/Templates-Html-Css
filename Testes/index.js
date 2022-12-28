7f3d2721ac1be6bd00db180e0768d64de7f1b010
Reginaldo Assunção <regi.assuncao@hotmail.com>
2022-12-28 20:46:40 -0300

    arquivos modificados

--- Testes/index.js
+++ Testes/index.js
@@ -1,20 +1,8 @@
-let linguagem = "JavaScript";
-let stack = "back-end";
+let number1 = 2;
+let number2 = 2;
+let number = number1 * number2
 
-switch (stack) {
-  case "back-end":
-    console.log(`Estude Node.js para programação ${ stack } em ${ linguagem }`);
-    break;
-
-  case "front-end":
-    console.log(`Estude React para programação ${ stack } em ${ linguagem }`);
-    break;
-
-  case "mobile":
-    console.log(`Estude React Native para programação ${ stack } em ${ linguagem }`);
-    break;
-
-  default:
-    console.log(`Tecnologia indisponível para ${ stack }`);
-    break;
+while(number < Infinity){
+    number = number * number
+    console.log(number)
 }
